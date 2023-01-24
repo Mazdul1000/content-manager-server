c972da0d6c9f92827c7dacfe7bc368abbbea58dc
mazdul1000 <mazdul1000@gmail.com>
2023-01-25 01:00:31 +0600

    fixing error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://js-hub:N0i105kmwXIiR6Pn@js-hub.txq7wxp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@js-hub.txq7wxp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
